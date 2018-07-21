b3544ba3bc86d9dd2d5aea172d06bb3e8c508051
Zuodion <zuodion@gmail.com>
2018-07-21 11:46:02 +0300

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -54,6 +54,23 @@ app.get('/todos/:id', (req, res) => {
         res.status(400).send();
     });
 });
+
+app.delete('/todos/:id', (req, res) => {//удаляет документ с id, который посылается по delete запросу
+    let id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
