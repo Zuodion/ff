045dcc17ac21b233d5d544a4c61683150b7755a3
Zuodion <zuodion@gmail.com>
2018-07-24 17:12:42 +0300

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -97,9 +97,24 @@ app.patch('/todos/:id', (req, res) => { //кароче это все обнов�
     });
 });
 
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
+
+
 
 module.exports = {app};
 
