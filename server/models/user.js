2cf59ab91606c4fafaed008e7cdfe805c6b9cdbe
Zuodion <zuodion@gmail.com>
2018-07-25 17:33:44 +0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -104,6 +104,16 @@ UserSchema.pre('save', function (next) {
     }
 });//запустит код перед событием 'save'
 
+UserSchema.methods.removeToken = function (token) {
+    let user = this;
+
+    return user.update({
+        $pull: {//удаляет массивы по заданым критериям
+            tokens: {token}
+        }
+    })
+};
+
 let User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
